e1a9b4c053c5a7adf4728c4e4e32688cb651c40b
SammyKrosoft <sammy@hotmail.fr>
2020-01-16 08:01:44 -0500

    sanitize and ServerNameSubstring conditiongenerator

--- MonitoringScripts/In-House_Email_Services___Check_CPU_Health.ps1
+++ MonitoringScripts/In-House_Email_Services___Check_CPU_Health.ps1
@@ -12,7 +12,7 @@
 #$ServerListFile = "C:\Scripts\Daily_Checks\Scheduled_Tasks-EMailed_Results\Exchange_Servers_incl_2016.txt"
 #$ServerList = Get-Content $ServerListFile -ErrorAction SilentlyContinue 
 # Building search substring logic based on server names <change for your company/department>
-$ServerNamesSubstrings = @("EIS-*-EP0*","IMG-*-EP*","DCS-DCB-EP*")
+$ServerNamesSubstrings = @("*01","*02")
 $Logic = ""
 For ($i=0;$i -lt $ServerNamesSubstrings.Count;$i++){
     If ($i -ne $ServerNamesSubstrings.Count-1){
@@ -25,9 +25,9 @@ For ($i=0;$i -lt $ServerNamesSubstrings.Count;$i++){
 $GetExchangeServerCmd = "Get-ExchangeServer | where {$Logic -and ((`$_.ServerRole -like `"*HubTransport*`") -or (`$_.ServerRole -like `"Mailbox*`"))} | sort name | Select Name"
 $GetExchangeServerCmd
 $ServerList = Invoke-Expression $GetExchangeServerCmd
-#$ServerList = Invoke-Expression $GetExchangeServerCmd
-exit
-#$ServerList = Get-ExchangeServer | where {(($_.name -like "EIS-*-EP0*") -or ($_.name -like "IMG-*-EP*") -or ($_.name -like "DCS-DCB-EP*")) -and (($_.ServerRole -like "*HubTransport*") -or ($_.ServerRole -like "Mailbox*"))} | sort name | Select Name
+
+$ServerList
+
 
 
 $Result = @()
@@ -49,6 +49,7 @@ $Outputreport = "<HTML><TITLE> In-House Email Services - CPU Health Report </TIT
 
 ForEach($ComputerName in $ServerList) 
 {
+$ComputerName
 
 $AVGProc = $null
 $OS = $null
@@ -57,19 +58,16 @@ $timespan = $null
 $uptime = $null
 $vol = $null    
 
-$AVGProc = Get-WmiObject -computername $computername.Name win32_processor | 
-Measure-Object -property LoadPercentage -Average | Select Average
+$AVGProc = Get-WmiObject -computername $computername.Name win32_processor | Measure-Object -property LoadPercentage -Average | Select Average
 
-$OS = Get-WmiObject -Class win32_operatingsystem -computername $computername.Name |
-Select-Object @{Name = "MemoryUsage"; Expression = {"{0:N2}" -f ((($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)*100)/ $_.TotalVisibleMemorySize) }}
+$OS = Get-WmiObject -Class win32_operatingsystem -computername $computername.Name |Select-Object @{Name = "MemoryUsage"; Expression = {"{0:N2}" -f ((($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)*100)/ $_.TotalVisibleMemorySize) }}
 
 $OSInfo = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computername.Name -ErrorAction CONTINUE
 
 $timespan = $OSInfo.ConvertToDateTime($OSInfo.LocalDateTime) - $OSInfo.ConvertToDateTime($OSInfo.LastBootUpTime)
-            [int]$uptime = "{0:00}" -f $timespan.TotalHours
+[int]$uptime = "{0:00}" -f $timespan.TotalHours
                 
-$vol = Get-WmiObject -Class win32_Volume -ComputerName $computername.Name -Filter "DriveLetter = 'C:'" |
-Select-object @{Name = "C_PercentFree"; Expression = {"{0:N2}" -f  (($_.FreeSpace / $_.Capacity)*100) } }
+$vol = Get-WmiObject -Class win32_Volume -ComputerName $computername.Name -Filter "DriveLetter = 'C:'" |Select-object @{Name = "C_PercentFree"; Expression = {"{0:N2}" -f  (($_.FreeSpace / $_.Capacity)*100) } }
     
 $Result += [PSCustomObject] @{ 
         
@@ -80,10 +78,13 @@ $Result += [PSCustomObject] @{
         CDrive = "$($vol.'C_PercentFree')"
          
     }}
+
                
         Foreach($Entry in $Result) 
-    
-        {  if(([decimal]$Entry.UpTime -le 168) -and ([decimal]$Entry.CPULoad -lt 90) -and ([decimal]$Entry.MemLoad -lt 99) -and ([decimal]$Entry.CDrive -gt 8))
+            
+        {  
+            $Entry
+            if(([decimal]$Entry.UpTime -le 168) -and ([decimal]$Entry.CPULoad -lt 90) -and ([decimal]$Entry.MemLoad -lt 99) -and ([decimal]$Entry.CDrive -gt 8))
 
             {$Outputreport += "<TR bgcolor=Yellow>"
              $WarningEvents ++}
@@ -100,7 +101,7 @@ $Result += [PSCustomObject] @{
           
    $Outputreport += "</Table></BODY></HTML>"
 
-$Outputreport | out-file C:\Scripts\Daily_Checks\Scheduled_Tasks-EMailed_Results\In-House_Email_Services___CPU_Health_Report.htm
+$Outputreport | out-file C:\Scripts\MonitoringScripts\In-House_Email_Services___CPU_Health_Report.htm
 #Invoke-Expression C:\Scripts\Daily_Checks\Scheduled_Tasks-EMailed_Results\In-House_Email_Services___CPU_Health_Report.htm
 
 ##Send email functionality from below line, use it if you want
@@ -113,15 +114,13 @@ elseif ($WarningEvents -gt 0)
 else
     {$EmailPriority = "Low"}
 
-$EMailAttachment = "C:\Scripts\Daily_Checks\Scheduled_Tasks-EMailed_Results\In-House_Email_Services___CPU_Health_Report.htm"
+$EMailAttachment = "C:\Scripts\MonitoringScripts\In-House_Email_Services___CPU_Health_Report.htm"
 
-$SMTPServer1 = "EIS-LS2-EP06143"
-$SMTPServer2 = "EIS-LS2-EP06144"
-$SMTPServer3 = "EIS-DCB-EP06157"
+$SMTPServer1 = "E2016-01"
+$SMTPServer2 = "E2016-02"
 
-$From = "Admin@forces.gc.ca"
-$Recipient = "DEMSCEMOPS@forces.gc.ca"
-#$Recipient = "kwok-fai.ha@ssc-spc.gc.ca"
+$From = "Administrator@Canadasam.ca"
+$Recipient = "Administrator@CanadaSam.ca"
 
 $Subject = "In-House Email Services - CPU Health Report - $ReportDate"
 
@@ -136,3 +135,4 @@ elseif (Test-Connection $SMTPServer2 -Quiet)
 else 
  
 {Send-MailMessage -from $From -to $Recipient -subject $Subject -Priority $EmailPriority -SMTPServer $SMTPServer3 -Attachments $EMailAttachment}
+
